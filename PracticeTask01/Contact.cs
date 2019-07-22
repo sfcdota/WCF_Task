c7a2fa1976b3e7266ba92ee9d9f5a8f0e7955961
sfcdota <sfcdota@gmail.com>
2019-07-23 00:46:01 +0300

    Добавьте файлы проекта.

---
+++ PracticeTask01/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask01
+{
+    [ClassDescription(Description = "Контакт")]
+    class Contact
+    {
+        private string surname,
+        name,
+        patronymic,
+        taxpayerIdentificationNumber,
+        Position;
+        private char sex;
+        private System.DateTime birthDate;
+        
+    }
+}
