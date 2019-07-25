6130fd75a92734fdb189563ed52d4771e3aed7a1
sfcdota <32439134+sfcdota@users.noreply.github.com>
2019-07-25 10:09:06 +0300

    ss

---
+++ ContactDLL/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactDLL
+{
+    public abstract class Person
+    {
+        private string _Surname;
+        private string _Name;
+        private string _Patronymic;
+        private string _Sex;
+    }
+}
